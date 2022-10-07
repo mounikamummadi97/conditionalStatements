e987e44f3c4b8f18871ee8294bca00c8496fbcad
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-07 15:07:55 +0530

    initial commit

---
+++ conditional statements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace myApp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 10;
+            if (x > y)
+                Console.WriteLine("x is greater than y");
+            else
+                Console.WriteLine("y is less than x");
+            int time = 12;
+            if (time < 10)
+            {
+                Console.WriteLine("good morning");
+            }
+            else  if (time > 12)
+                {
+                Console.WriteLine("good afernoon");
+                }
+            else
+            {
+                Console.WriteLine("none");
+            }
+            }
+        }
+    }
